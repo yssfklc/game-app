7aeb32a2080c71977ca5f2db2c0072ddec0e7fb8
Yusuf Kılıç <y.kilic@superpay.tech>
2023-09-18 19:41:22 +0300

    add about us page

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import g18 from './image/g18.png';
+import g19 from './image/g19.png';
+import i7 from './image/i7.svg';
+import i8 from './image/i8.svg';
+import i9 from './image/i9.svg';
+import i10 from './image/i10.svg';
 
 function Aboutus() {
+    const employees=[[i7, 'John peter', 'COO'],[i8, 'John peter', 'COO'],[i9, 'John peter', 'COO'],[i10, 'John peter', 'COO']]
   return (
-    <div className='grid grid-cols-12 grid-rows-6 gap-10'>
+    <div className='grid grid-cols-12 grid-rows-4 gap-10'>
         <div className='col-start-2 col-span-10 row-start-1 row-span-1 flex items-center mt-20'>
             <div className='w-1/2 flex flex-col justify-center p-10'>
                 <h1 className='text-gray-100 text-4xl mb-8 font-extrabold leading-relaxed	w-3/4 font-serif'>Work that we produce for our clients</h1>
@@ -18,17 +24,69 @@ function Aboutus() {
             </div>
         </div>
         <div className='col-start-2 col-span-10 row-start-2 row-span-1'>
-            <div className='my-5'>
+            <div className='text-gray-100 mt-20 mb-20 font-extrabold text-xl leading-8 tracking-wider'>
                 <h2>Why Work Wıth Us</h2>
             </div>
-            <div className=''>
-                <div className='w-1/4 bg-gray-300 py-5 px-2 flex flex-col justify-around items-start rounded-xl'>
-                    <p>Lorem ipsum</p>
+            <div className='flex justify-between'>
+                <div className='w-96 bg-gray-100 py-10 h-80 px-10 flex flex-col justify-around items-start rounded-xl'>
+                    <p className='bg-pink-100 rounded-sm px-3 py-1'>Lorem ipsum</p>
+                    <h3 className='text-xl font-bold'>Lorem Ipsum</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                </div>
+                <div className='w-96 bg-gray-100 py-10 h-80 px-10 flex flex-col justify-around items-start rounded-xl'>
+                    <p className='bg-orange-100 rounded-sm px-3 py-1'>Lorem ipsum</p>
+                    <h3 className='text-xl font-bold'>Lorem Ipsum</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                </div>
+                <div className='w-96 bg-gray-100 py-10 h-80 px-10 flex flex-col justify-around items-start rounded-xl'>
+                    <p className='bg-green-100 rounded-sm px-3 py-1'>Lorem ipsum</p>
                     <h3 className='text-xl font-bold'>Lorem Ipsum</h3>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
                 </div>
             </div>
         </div>
+        <div className='col-start-1 col-span-12 row-start-3 row-span-1 bg-gray-100'>
+            
+        </div>
+        <div className='col-start-2 col-span-10 row-start-3 row-span-1 flex'>
+            <div className='w-1/2 flex items-end justify-start'>
+                <img src={g19} alt=''/>
+            </div>
+            <div className='w-1/2 flex flex-col justify-end items-start p-10'>
+                <p className='text-gray-800 mb-20 font-bold'>Lorem Ipsum</p>
+                <h1 className='text-gray-800 text-4xl mb-8 font-extrabold leading-relaxed	w-3/4 font-serif'>Work that we produce for our clients</h1>
+                <p className='text-gray-800 mb-20'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p> 
+            </div>
+        </div>
+        <div className='col-start-2 col-span-10 row-start-4 row-span-1 '>
+            <p className='font-bold my-20 text-gray-100 text-xl'>Our Team</p>
+            <div className='flex justify-center items-center mb-20'>
+                {employees.map((employee)=>{
+                    return (<div className='w-1/4 bg-gray-800 mr-5 py-10 px-5 rounded-xl h-70 flex flex-col items-center justify-center'>
+                        <img src={employee[0]}/>
+                        <p className='text-gray-100'>{employee[1]}</p>
+                        <p className='text-gray-100'>{employee[2]}</p>
+                    </div>)
+
+                })}
+            </div>
+                <div className='w-1/2  mb-10'>
+                <h3 className='text-gray-100 text-lg font-bold mb-3'>Lorem Ipsum</h3>
+                <p className='text-gray-100'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              </div>
+              <div className='bg-gray-200 rounded-lg py-10 px-5 flex justify-between items-center mb-20'>
+                    <div className='w-1/2'>
+                    <h3 className='text-black text-xl font-extrabold mb-3 leading-7 tracking-wider'>Lorem Ipsum</h3>
+                    <p className='text-black'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                    <div className='flex rounded-lg px-3 py-2 bg-gray-800 items-center justify-center'>
+                    <input className='bg-gray-800 mr-2 text-gray-100 font-light' placeholder='Type Your Email'/>
+                    <button type='submit' className='bg-red-800 py-1 px-3 rounded-lg text-gray-800 inline-flex justify-center'>Continue</button>
+                    </div>
+                </div>
+        </div>
+             
+
     </div>
   )
 }
